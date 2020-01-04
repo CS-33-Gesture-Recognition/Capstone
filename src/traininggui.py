82c365529333e106c8798f1fbc94ad7d3f542144
Shane Clancy <clancys@oregonstate.edu>
2020-01-03 18:09:25 -0800

    Added onClick and onChange events for Widgets.

--- src/traininggui.py
+++ src/traininggui.py
@@ -1,26 +1,41 @@
+#PyQt imports
 from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLineEdit 
+from PyQt5.QtCore import QThread
+
 import sys
 import datacollection as dc
 
 class Container(QWidget):
-    
+
     def __init__(self):
         super().__init__();
+        self.__textField = "";
         self.initUI();
 
     def initUI(self):
 
-        contents = "";
-        inputText = QLineEdit(contents, self);
+        inputText = QLineEdit(self.__textField, self);
         inputText.setToolTip('Input Gesture Classification Here');
         inputText.resize(150, 100);
+        inputText.textChanged.connect(self.onTextChange);
 
         captureButton = QPushButton('Capture Data', self);
         captureButton.setToolTip('Use this button to capture an image from camera');
         captureButton.resize(150, 100);
         captureButton.move(150, 0);
+        captureButton.clicked.connect(self.onCaptureClick);
 
-    def onCaptureClick():
+    def onTextChange(self, text):
+        self.__textField = text;
+        return;
+
+
+    def onCaptureClick(self):
+        dc.gatherCameraImage();
+        dc.outputClassification(self.__textField);
+        print("self.__textField ", self.__textField);
+        return;
+
 
 
 def main():
