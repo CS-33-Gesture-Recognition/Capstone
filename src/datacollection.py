76eef36becfb5c97fd91c530355793c01496cc84
Shane Clancy <clancys@oregonstate.edu>
2020-01-21 22:05:04 -0800

    update collectTestingX and test py file

--- src/datacollection.py
+++ src/datacollection.py
@@ -160,3 +160,56 @@ def collectTrainingY():
             for y in train_y:
                 train_y_data.append(y.decode("utf-8"));
             return train_y_data;
+
+def collectTestingX():
+    pipeline = rs.pipeline()
+
+    #Create a config and configure the pipeline to stream
+    #  different resolutions of color and depth streams
+    config = rs.config()
+    config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
+
+     # Start streaming
+    profile = pipeline.start(config)
+
+    # Getting the depth sensor's depth scale (see rs-align example for explanation)
+    depth_sensor = profile.get_device().first_depth_sensor()
+    depth_scale = depth_sensor.get_depth_scale()
+
+    # We will be removing the background of objects more than
+    #  clipping_distance_in_meters meters away
+    clipping_distance_in_meters = 1 #1 meter
+    clipping_distance = clipping_distance_in_meters / depth_scale
+
+    # Create an align object
+    # rs.align allows us to perform alignment of depth frames to others frames
+    # The "align_to" is the stream type to which we plan to align depth frames.
+    align_to = rs.stream.color
+    align = rs.align(align_to)
+
+    # Get frameset of color and depth
+    frames = pipeline.wait_for_frames()
+    # frames.get_depth_frame() is a 640x360 depth image
+
+     # Align the depth frame to color frame
+    aligned_frames = align.process(frames)
+
+    # Get aligned frames
+    aligned_depth_frame = aligned_frames.get_depth_frame() # aligned_depth_frame is a 640x480 depth image
+    color_frame = aligned_frames.get_color_frame()
+
+    depth_image = np.asanyarray(aligned_depth_frame.get_data())
+    color_image = np.asanyarray(color_frame.get_data())
+
+    # Remove background - Set pixels further than clipping_distance to grey
+    grey_color = 153
+    depth_image_3d = np.dstack((depth_image,depth_image,depth_image)) #depth image is 1 channel, color is 3 channels
+    bg_removed = np.where((depth_image_3d > clipping_distance) | (depth_image_3d <= 0), grey_color, color_image)
+
+    depth_image = normalizeDepthImage(depth_image);
+
+    pipeline.stop();
+
+    return depth_image;
+
+
--- src/test.py
+++ src/test.py
@@ -4,21 +4,9 @@ import numpy as np;
 
 
 def main():
-    x = dc.collectTrainingX();
-    y = dc.collectTrainingY();
 
-    print('y size: ', len(y));
-    print('x dims: ', x.shape);
-
-    print('y: ', y);
-    print("x: ", x);
-
-    ex_x = [np.array([[1.0,2],[3,4]]),np.array([[5.,6],[7,8]])] # a list of numpy arrays
-    ex_y = [np.array([4.]), np.array([2.])]
-
-
-    print('ex_y: ', ex_y);
-    print("ex_x: ", ex_x);
+    test_x = dc.collectTestingX();
+    print(test_x);
 
 
 if __name__ == "__main__":
