493bb07fe89ff9664fb77236390326f9d1fc5385
Shane Clancy <clancys@oregonstate.edu>
2020-03-19 19:49:54 -0700

    #11 #12 Updating SW to capture more efficient depth colormap images and save them to specified folders.

--- src/datacollection.py
+++ src/datacollection.py
@@ -21,117 +21,24 @@ import os
 # import image lib to save images
 from PIL import Image
 
-TRAINX_SINGLE_DATA_SIZE = 307200;
-
-def invertDepth(depth_image):
-    largest = 0.0;
-    depth_image_inverted = [];
-
-    for x in range(len(depth_image)):
-        for i in range(len(depth_image[x])):
-            if (float(depth_image[x][i]) > largest):
-                largest = float(depth_image[x][i]);
-
-    for x in range(len(depth_image)):
-        temp = [];
-        for i in range(len(depth_image[x])):
-            if (depth_image[x][i] != 0.0):
-                temp.append(largest - depth_image[x][i]);
-                depth_image[x][i] = temp[i];
-            else:
-                temp.append(depth_image[x][i]);
-                depth_image[x][i] = temp[i];
-        depth_image_inverted.append(temp);
-
-    return np.asanyarray(depth_image_inverted);
-
-def filterDepth(depth_image):
-
-    smallest = 50000.0;
-    depth_image_filtered = [];
-
-    for x in range(len(depth_image)):
-        for i in range(len(depth_image[x])):
-            if (float(depth_image[x][i]) < smallest and float(depth_image[x][i]) != 0):
-                smallest = float(depth_image[x][i]);
-
-    if (smallest == 50000.0):
-        smallest = 0.0;
-    
-    for x in range(len(depth_image)):
-        temp = [];
-        for i in range(len(depth_image[x])):
-            if (depth_image[x][i] != 0.0):
-                temp.append(depth_image[x][i] - smallest);
-                depth_image[x][i] = temp[i];
-            else:
-                temp.append(depth_image[x][i]);
-                depth_image[x][i] = temp[i];
-        depth_image_filtered.append(temp);
-
-    return np.asanyarray(depth_image_filtered);
-
-# function used to normalize data from all real numbers to [0,1]
-def normalizeDepthImage(depth_image):
-
-    largest = 0.0;
-    depth_image_normalized = [];
-
-    for x in range(len(depth_image)):
-        for i in range(len(depth_image[x])):
-            if (float(depth_image[x][i]) > largest):
-                largest = float(depth_image[x][i]);
-
-    for x in range(len(depth_image)):
-        temp = [];
-        for i in range(len(depth_image[x])):
-            if (largest != 0.0):
-                temp.append(float(depth_image[x][i]) / float(largest));
-                depth_image[x][i] = temp[i];
-        depth_image_normalized.append(temp);
-
-    return np.asanyarray(depth_image_normalized);
-
 # function that outputs training data from camera to dataset.
-def outputData(depth_image):
-    print("Outputting Depth Image\n")
+def outputData(depth_colormap, gesture):
+    strPath = './datasets/' + gesture + '/';
+    
+    if (not os.path.exists(strPath)):
+        os.mkdir(strPath);
+    
+    files = os.listdir(strPath);
 
-    # flatten image for easier chunking [[]] -> []
-    depth_image_flat = [];
-    for x in depth_image:
-        for y in x:
-            depth_image_flat.append(y);
-    depth_image_flat = np.asanyarray(depth_image_flat);
+    idx = len(files);
 
-    # if dataset exists in path, append new depth image to it
-    if ((os.path.isfile('datasets/train_x.hdf5'))):
-        print("Appending to train_x dataset");
-        with h5py.File('datasets/train_x.hdf5', 'a') as train_x:
-            train_x["train_x"].resize((train_x["train_x"].shape[0] + depth_image_flat.shape[0]), axis=0);
-            train_x["train_x"][-depth_image_flat.shape[0]:] = depth_image_flat;
-    else:
-        print("Creating new train_x dataset");
-        with h5py.File('datasets/train_x.hdf5', 'w') as train_x:
-            # creates new dataset, with gzip compression and chunking
-            dset = train_x.create_dataset("train_x", data=depth_image_flat, compression="gzip", chunks=True, maxshape=(None,));
-
-# function that outputs classification from GUI to dataset.
-def outputClassification(text):
-    #Convert text to dataset for append.
-    textAsDataSet = np.asanyarray([text.lower().encode("ascii","ignore")]);
-
-    if (os.path.isfile('datasets/train_y.hdf5')):
-        print("Appending to train_y dataset");
-        with h5py.File('datasets/train_y.hdf5', 'a') as train_y:
-            train_y["train_y"].resize((train_y["train_y"].shape[0] + textAsDataSet.shape[0]), axis=0);
-            train_y["train_y"][-textAsDataSet.shape[0]:] = textAsDataSet;
-    else:
-        print("Creating train_y dataset");
-        with h5py.File('datasets/train_y.hdf5', 'w') as train_y:
-            train_y.create_dataset("train_y", data=textAsDataSet, compression="gzip", dtype='S10', maxshape=(None,));
+    depth_colormap_image = Image.fromarray(depth_colormap);
+    image_path = strPath + str(idx) + '.jpg';
+    print(image_path);
+    depth_colormap_image.save(image_path);
 
 # function that gathers camera image data, displays it, and asks for classification
-def gatherCameraImage():
+def gatherCameraImage(gesture, iterations):
     # Create a pipeline
     pipeline = rs.pipeline()
 
@@ -139,82 +46,55 @@ def gatherCameraImage():
     #  different resolutions of color and depth streams
     config = rs.config()
     config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
-    config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
 
     # Start streaming
     profile = pipeline.start(config)
 
-    # Getting the depth sensor's depth scale (see rs-align example for explanation)
-    depth_sensor = profile.get_device().first_depth_sensor()
-    depth_scale = depth_sensor.get_depth_scale()
+    for iteration in range(iterations):
 
-    # We will be removing the background of objects more than
-    #  clipping_distance_in_meters meters away
-    clipping_distance_in_meters = 1 #1 meter
-    clipping_distance = clipping_distance_in_meters / depth_scale
+        # Getting the depth sensor's depth scale (see rs-align example for explanation)
+        depth_sensor = profile.get_device().first_depth_sensor()
+        depth_scale = depth_sensor.get_depth_scale()
 
-    # Create an align object
-    # rs.align allows us to perform alignment of depth frames to others frames
-    # The "align_to" is the stream type to which we plan to align depth frames.
-    align_to = rs.stream.color
-    align = rs.align(align_to)
+        # We will be removing the background of objects more than
+        #  clipping_distance_in_meters meters away
+        clipping_distance_in_meters = 1 #1 meter
+        clipping_distance = clipping_distance_in_meters / depth_scale
 
-    # Get frameset of color and depth
-    frames = pipeline.wait_for_frames()
-    # frames.get_depth_frame() is a 640x360 depth image
+        # Create an align object
+        # rs.align allows us to perform alignment of depth frames to others frames
+        # The "align_to" is the stream type to which we plan to align depth frames.
+        align_to = rs.stream.color
+        align = rs.align(align_to)
 
-    # Align the depth frame to color frame
-    aligned_frames = align.process(frames)
+        # Get frameset of color and depth
+        frames = pipeline.wait_for_frames()
+        # frames.get_depth_frame() is a 640x360 depth image
 
-    # Get aligned frames
-    aligned_depth_frame = aligned_frames.get_depth_frame() # aligned_depth_frame is a 640x480 depth image
-    color_frame = aligned_frames.get_color_frame()
+        # Align the depth frame to color frame
+        aligned_frames = align.process(frames)
 
-    depth_image = np.asanyarray(aligned_depth_frame.get_data())
-    color_image = np.asanyarray(color_frame.get_data())
+        # Get aligned frames
+        aligned_depth_frame = aligned_frames.get_depth_frame() # aligned_depth_frame is a 640x480 depth image
+        # color_frame = aligned_frames.get_color_frame()
 
-    # Remove background - Set pixels further than clipping_distance to grey
-    grey_color = 153
-    depth_image_3d = np.dstack((depth_image,depth_image,depth_image)) #depth image is 1 channel, color is 3 channels
-    bg_removed = np.where((depth_image_3d > clipping_distance) | (depth_image_3d <= 0), grey_color, color_image)
+        depth_image = np.asanyarray(aligned_depth_frame.get_data())
+        # color_image = np.asanyarray(color_frame.get_data())
 
-    # Render images
-    depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET)
-    images = np.hstack((bg_removed, depth_colormap))
-    cv2.namedWindow('Align Example', cv2.WINDOW_AUTOSIZE)
-    cv2.imshow('Align Example', images)
-    key = cv2.waitKey(1)
+        # Remove background - Set pixels further than clipping_distance to grey
+        grey_color = 153
+        depth_image_3d = np.dstack((depth_image,depth_image,depth_image)) #depth image is 1 channel, color is 3 channels
 
-    #Invert depth image
-    depth_image = invertDepth(depth_image);
-    #Filter depth image
-    depth_image = filterDepth(depth_image);
-    #Normalize depth image
-    depth_image = normalizeDepthImage(depth_image);
+        # Render images
+        depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET)
+        images = np.hstack((depth_colormap, depth_colormap))
+        cv2.namedWindow('Align Example', cv2.WINDOW_AUTOSIZE)
+        cv2.imshow('Align Example', images)
+        key = cv2.waitKey(1)
 
-    #Writing depth image to file
-    outputData(depth_image);
+        outputData(depth_colormap,  gesture);
 
     pipeline.stop()
-
-def collectTrainingX():
-    if (os.path.exists('./datasets/train_x.hdf5')):
-        with h5py.File('./datasets/train_x.hdf5', 'r') as train_x_file:
-            train_x = train_x_file['train_x'];
-            train_x_data = np.reshape(train_x, (int(len(train_x)/TRAINX_SINGLE_DATA_SIZE), TRAINX_SINGLE_DATA_SIZE));
-            reshaped_x = [];
-            for i in train_x_data:
-                reshaped_x.append(np.reshape(i, (480, 640)));
-            return reshaped_x;
-
-def collectTrainingY():
-    if (os.path.exists('./datasets/train_y.hdf5')):
-        with h5py.File('./datasets/train_y.hdf5', 'r') as train_y_file:
-            train_y = train_y_file['train_y'];
-            train_y_data = [];
-            for y in train_y:
-                train_y_data.append(y.decode("utf-8"));
-            return train_y_data;
 
 def collectTestingX():
     # Create a pipeline
@@ -223,8 +103,8 @@ def collectTestingX():
     #Create a config and configure the pipeline to stream
     #  different resolutions of color and depth streams
     config = rs.config()
-    config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
-    config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
+    config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30);
+    config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30);
 
     # Start streaming
     profile = pipeline.start(config)
@@ -263,39 +143,20 @@ def collectTestingX():
     depth_image_3d = np.dstack((depth_image,depth_image,depth_image)) #depth image is 1 channel, color is 3 channels
     bg_removed = np.where((depth_image_3d > clipping_distance) | (depth_image_3d <= 0), grey_color, color_image)
 
-    # Render images
     depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET)
-    images = np.hstack((bg_removed, depth_colormap))
 
     outputDataToFileStructure(depth_colormap, bg_removed);
-    #Invert depth image
-    depth_image = invertDepth(depth_image);
-    #Filter depth image
-    depth_image = filterDepth(depth_image);
-    #Normalize depth image
-    depth_image = normalizeDepthImage(depth_image);
-
 
     pipeline.stop()
 
     return depth_image;
 
 def outputDataToFileStructure(depth_image, color_image):
-    #now = datetime.now()  # current date and time
-    #date_time = now.strftime("%m_%d_%Y__%H_%M_%S")
-    # print("depth_image: ", depth_image)
-    # print("text: ", label)
-    # file_path = 'trainingSets/' # + label
-
-    # if not os.path.exists(file_path):
-    #     print("Creating filepath: ", file_path)
-    #     os.makedirs(file_path)
-    # else:
-    #     print(file_path, " already exists")
-
-    #Arbitrary name to be overwritten for testing purposes
-    # file_path = file_path + '/' + 'color_test.png' 
     img = Image.fromarray(color_image, 'RGB')
     img.save("rgb_image.jpg")
     img = Image.fromarray(depth_image, 'RGB')
     img.save("depth_image.jpg")
+
+def getNumberLabels():
+    dirs = os.listdir('./datasets/');
+    return len(dirs);
