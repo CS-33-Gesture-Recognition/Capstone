a711fbab55459a810a90d908d2cffd11f99cab99
Travis Seevers <seeverst@oregonstate.edu>
2020-11-13 06:02:35 -0800

    Added more comments to the preproc file

--- src/code/PP/preproc.py
+++ src/code/PP/preproc.py
@@ -33,17 +33,31 @@ class PreProc:
         self.align = rs.align(align_to)
 
     def fix_scale(self, image):
+        # First, find the minimum value in the depth image
+        # for the depth image, the closer the object is to the camera, the smaller
+        # the value. Thus, we want to find the closest pixel
         minVal = np.min(image[np.nonzero(image)])
         
-        image[image > 1500 + minVal] = 1
+        # Now we want to mask off the background. We do this by
+        # setting any pixel thats further away than 1500 units from
+        # the closest pixel to zero
+        image[image > 1500 + minVal] = 0.
 
+        # After masking off the background, find the furthest distance in our ROI
+        # (note, this could be minVal + 1500, but it could be smaller)
         maxVal = np.max(image[np.nonzero(image)])
 
-        # image[image == 0] = maxVal
-
-        # lets aim for a range of 0 -> 1000
+        # Now we preform two operations at once, the first is to scale the ROi relitive to itself
+        # as such, the closest pixel should be near zero, and the furthest near one.
+        # Second, we raise this value to the fourth power, this is to help make minor differences
+        # between pixels more distinct.
+        # For example: the sign 'A' versus the sign 'S'
+        # 'A' has the thumb closer to the camera than in 'S', but the difference is thousanths of units 
         new_img = (image / maxVal) ** 4
 
+        # Any small value (arbitrarily defined as smaller than 0.001, typically in the range ~E-5) is the 
+        # result of floating point errors with the masked off pixels (I beleive)
+        # Set these background pizels as 1, the furthest away in our range
         new_img[new_img < 0.001] = 1
 
         return new_img
