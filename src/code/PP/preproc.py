1165e5cb913febca5ce1a4d1bcdc4abf4aee0c67
Travis Seevers <seeverst@oregonstate.edu>
2020-11-13 06:29:04 -0800

    Fixed even more linting issues

--- src/code/PP/preproc.py
+++ src/code/PP/preproc.py
@@ -1,9 +1,9 @@
 import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
-import matplotlib.colors as color
+# import matplotlib.image as mpimg
+# import matplotlib.colors as color
 import pyrealsense2 as rs
 import numpy as np
-import time
+# import time
 
 
 # make the matplotlib plots interactive, this allows them to
@@ -34,29 +34,37 @@ class PreProc:
 
     def fix_scale(self, image):
         # First, find the minimum value in the depth image
-        # for the depth image, the closer the object is to the camera, the smaller
+        # for the depth image, the closer the object is to the camera,
+        #  the smaller
         # the value. Thus, we want to find the closest pixel
         minVal = np.min(image[np.nonzero(image)])
-        
+
         # Now we want to mask off the background. We do this by
         # setting any pixel thats further away than 1500 units from
         # the closest pixel to zero
         image[image > 1500 + minVal] = 0.
 
-        # After masking off the background, find the furthest distance in our ROI
+        # After masking off the background, find the furthest distance
+        # in our ROI
         # (note, this could be minVal + 1500, but it could be smaller)
         maxVal = np.max(image[np.nonzero(image)])
 
-        # Now we preform two operations at once, the first is to scale the ROi relitive to itself
-        # as such, the closest pixel should be near zero, and the furthest near one.
-        # Second, we raise this value to the fourth power, this is to help make minor differences
+        # Now we preform two operations at once, the first is to scale the ROi
+        # relitive to itself
+        # as such, the closest pixel should be near zero, and the furthest
+        # near one.
+        # Second, we raise this value to the fourth power, this is to help
+        #  make minor differences
         # between pixels more distinct.
         # For example: the sign 'A' versus the sign 'S'
-        # 'A' has the thumb closer to the camera than in 'S', but the difference is thousanths of units 
+        # 'A' has the thumb closer to the camera than in 'S', but the
+        #  difference is thousanths of units
         new_img = (image / maxVal) ** 4
 
-        # Any small value (arbitrarily defined as smaller than 0.001, typically in the range ~E-5) is the 
-        # result of floating point errors with the masked off pixels (I beleive)
+        # Any small value (arbitrarily defined as smaller than 0.001,
+        #  typically in the range ~E-5) is the
+        # result of floating point errors with the masked off pixels
+        # (I beleive)
         # Set these background pizels as 1, the furthest away in our range
         new_img[new_img < 0.001] = 1
 
@@ -76,7 +84,7 @@ class PreProc:
 
         # Get aligned frames
         # aligned_depth_frame is a 640x480 depth image
-        aligned_depth_frame = aligned_frames.get_depth_frame() 
+        aligned_depth_frame = aligned_frames.get_depth_frame()
 
         # create the mono-color image as a np array
         img = np.asanyarray(aligned_depth_frame.get_data())
@@ -93,7 +101,7 @@ class PreProc:
         plt.show()
 
         # Matplotlib only updates the plots when the program is idle
-        # thus, having the program preform a slight pause will ensure the 
+        # thus, having the program preform a slight pause will ensure the
         # plot is updated
         plt.pause(0.001)
 
